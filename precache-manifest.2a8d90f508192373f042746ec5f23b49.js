699c2600f0fec5b0f8ba6a95c5dfbb217b3cb7e0
santhosh edupalli <evssanthosh@gmail.com>
2020-07-20 14:57:33 +0530

    Updates

---
+++ precache-manifest.2a8d90f508192373f042746ec5f23b49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ba90307d771734b5a5d472654951f5",
+    "url": "/kaboom-dashboard/index.html"
+  },
+  {
+    "revision": "e833148d1f74a62fa2a4",
+    "url": "/kaboom-dashboard/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "9e4ac6e0ea3104128420",
+    "url": "/kaboom-dashboard/static/css/main.204c71e0.chunk.css"
+  },
+  {
+    "revision": "e833148d1f74a62fa2a4",
+    "url": "/kaboom-dashboard/static/js/2.7632ab6a.chunk.js"
+  },
+  {
+    "revision": "f68cbcd06da434c881f4d4bbf2fcec58",
+    "url": "/kaboom-dashboard/static/js/2.7632ab6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4ac6e0ea3104128420",
+    "url": "/kaboom-dashboard/static/js/main.c4cfb8e7.chunk.js"
+  },
+  {
+    "revision": "369276bb32615933e6a5",
+    "url": "/kaboom-dashboard/static/js/runtime-main.f46c0eee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaboom-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
